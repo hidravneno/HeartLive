693fd9c4da841942e97e6149606513f1cf4cf26c
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-11-25 17:12:50 -0800

    Initial Commit

---
+++ HeartLive Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartLive Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartLive Watch App/HeartLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLiveApp.swift
+//  HeartLive Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartLive_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
